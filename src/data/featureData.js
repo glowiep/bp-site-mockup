8bbb8acd1c671abfe6787ecfcbc44e2b9704c212
Gloria Lim <glorialimartt@gmail.com>
2024-03-22 20:37:17 -0400

    Update image links

--- src/data/featureData.js
+++ src/data/featureData.js
@@ -5,7 +5,7 @@ export const featureData = [
     description: "Our easy-to-use tools help you to navigate common daily tasks such as managing routes, assigning busing, and creating reports. Our powerful optimization toolkit offers 'what-if' scenarios to build your best transportation solution. Integrated analysis tools help you evaluate options and make better decisions.",
     url: "https://www.busplanner.com/planning.htm?v1711072207342",
     urlDescription: "More Product Information",
-    image: "docs/1_How_Can_BusPlanner_Help.svg"
+    image: "https://raw.githubusercontent.com/glowiep/bp-site-mockup/c497f20315ba27f2ce9cfb15e50aca5c0613761f/public/docs/1_How_Can_BusPlanner_Help.svg"
   },
   {
     id: 1,
@@ -13,7 +13,7 @@ export const featureData = [
     description: "More efficient school bus routes save everyone time and money. The BusPlanner Suite equips you with tools to plan your routes, analyze your solution, and optimize your operation for greater efficiency. Whether you manage 20 or 2,000 bus routes, our scalable solution can help you.",
     url: "https://www.busplanner.com/services.htm?v1711072218495",
     urlDescription: "How We Can Help",
-    image: "../../public/docs/2_Save_Time_and_Money.svg"
+    image: "https://raw.githubusercontent.com/glowiep/bp-site-mockup/c497f20315ba27f2ce9cfb15e50aca5c0613761f/public/docs/2_Save_Time_and_Money.svg"
   },
   {
     id: 2,
@@ -21,6 +21,6 @@ export const featureData = [
     description: "We are here to support you and your team every step of the way. Our BusPlanner Help Desk offers live, knowledgeable technical support from 7:00am-7:00pm ET Monday to Friday. Our friendly help desk associates are committed to helping your team ensure your success.",
     url: "https://www.busplanner.com/press.htm?v1711072224602",
     urlDescription: "Hear From Our Clients",
-    image: "docs/3_Unrivaled_Customer_Service.svg"
+    image: "https://raw.githubusercontent.com/glowiep/bp-site-mockup/c497f20315ba27f2ce9cfb15e50aca5c0613761f/public/docs/3_Unrivaled_Customer_Service.svg"
   }
 ]
