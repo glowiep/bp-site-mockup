e8009a3c164cd839130fb572c3a23d2a6d53e25c
Gloria Lim <glorialimartt@gmail.com>
2024-03-24 22:06:36 -0400

    Format with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import './App.scss'
-import Landing from './components/Landing'
-import YoutubeEmbed from './components/YoutubeEmbed'
-import Features from './components/Features'
-import Testimonials from './components/Testimonials'
-import GithubIcon from './components/GithubIcon'
-import NavigationBar from './components/NavigationBar'
+import "./App.scss";
+import Landing from "./components/Landing";
+import YoutubeEmbed from "./components/YoutubeEmbed";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import GithubIcon from "./components/GithubIcon";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  const youtubeEmbedId = "qvoiYbpkDfo"
+  const youtubeEmbedId = "qvoiYbpkDfo";
   return (
     <>
-      <section className='app'>
-        {/* <NavigationBar /> */}
+      <section className="app">
+        <NavigationBar />
         <Landing embedId={youtubeEmbedId} />
-        <section className='content'>
-          <YoutubeEmbed embedId={youtubeEmbedId} />  
+        <section className="content">
+          <YoutubeEmbed embedId={youtubeEmbedId} />
           <Features />
           <Testimonials />
         </section>
       </section>
-      
+
       <section className="read-me-note">
         <GithubIcon />
-        Created with ReactJS.<br />
-        <a href="https://www.busplanner.com/index.aspx" target="_blank" rel="noopener noreferrer">BusPlanner.com</a> website mockup by <br />
-        <a href="https://github.com/glowiep/bp-site-mockup" target="_blank" rel="noopener noreferrer">Gloria Lim</a>. 
+        Created with ReactJS.
+        <br />
+        <a
+          href="https://www.busplanner.com/index.aspx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          BusPlanner.com
+        </a>{" "}
+        website mockup by <br />
+        <a
+          href="https://github.com/glowiep/bp-site-mockup"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gloria Lim
+        </a>
+        .
       </section>
     </>
-  )
+  );
 }
 
 export default App;
